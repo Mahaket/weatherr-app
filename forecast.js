0c2a78874d7daa418c0f1ab65c8e836956e4f076
Mahaket <97586612+Mahaket@users.noreply.github.com>
2022-05-05 13:06:34 +0530

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=850cfda6651dc44a72e691481bd16488&query=+ encodeURIComponent(latitude, longitude) +&units=m'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
